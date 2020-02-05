c028d231c74e26a636da3a244326cfeb825eea84
Ldixuan <491358683@qq.com>
2020-02-06 02:09:18 +0800

    ajouter des commentaire

--- convNet.py
+++ convNet.py
@@ -49,7 +49,7 @@ def create_model():
     model.add(Activation('relu'))
 
     model.add(MaxPooling2D(pool_size = (2,2), data_format = "channels_first", padding = "same"))
-    model.add(Dropout(0.25)) #la courche de dropout
+    model.add(Dropout(0.25)) #la courche de dropout, Dropout: A Simple Way to Prevent Neural Networks from Overfitting 
 
     model.add(Conv2D(
         filters = 32,
